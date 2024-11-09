94e88c4d960d3029bd3d33d958a6835c9ec55e17
KittikarnIwai <s6506021622060@email.kmutnb.ac.th>
2024-11-10 01:05:51 +0700

    update Code

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 var bodyParser = require("body-parser");
+const axios = require("axios");
 const path = require("path");
 const app = express();
 const port = process.env.PORT || 5500;
@@ -14,6 +15,17 @@ app.use(express.static(path.join(__dirname, "/public")));
 
 app.get("/", (req, res) => {
     res.render("verify");
+});
+
+app.post("/verifyPost", async (req, res) => {
+    const data = req.body;
+    try {
+        await axios.post(`${base_url}/verifyPost`, data);
+        res.redirect("/");
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Error");
+    }
 });
 
 app.listen(port, () => {
